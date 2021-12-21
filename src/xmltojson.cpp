adc7a966170473d69259f9803b7667d7d3a57ccd
Toast732 <55813762+Toast732@users.noreply.github.com>
2021-12-21 14:41:21 -0500

    now correctly checks if a path is valid

--- src/xmltojson.cpp
+++ src/xmltojson.cpp
@@ -8,7 +8,6 @@ namespace fs = std::filesystem;
 // variables
 bool started = false;
 bool stop = false;
-class file_status;
 std::string version = "0.1";
 std::string commandPrefix = "-xmj ";
 std::string commands[2] = {"--getBlocks", "--getLogic"};
@@ -40,25 +39,21 @@ int start() {
 }
 
 int checkFile(bool correctPath) {
-    int attempts = 2;
+    int attempts = 1;
     while(correctPath == false) {
-        std::cout << "\nEnter the file path of stormworks\nDefault: C:\\Program Files\\Steam\\steamapps\\common\\Stormworks" << "\n>";
-        std::string swPath;
-        getline(std::cin, swPath);
-        try {
-            fs::path dir = swPath + defLoc;
-            std::cout << "\nChecking if " << dir << " is a valid directory...\n";
-            if(fs::exists(dir)) {
-                correctPath = true;
-                std::cout << "\nPath Valid!\n";
-            } else {
-                throw("Invalid Path!");
-            }
-        } catch (std::string err) {
-            std::cout << "\nError: " << err << "\n";
+        std::string path;
+        getline(std::cin, path);
+        fs::path dir = path + defLoc;
+        std::cout << "\nChecking if " << dir << " is a valid directory...\n";
+        if(fs::exists(dir)) {
+            correctPath = true;
+            std::cout << "\nPath Valid!\n";
+        } else {
+            std::cout << "\nError: " << "Invalid Path" << "\n\n";
             if(0 >= attempts) {
                 return false;
             } else {
+                std::cout << attempts << " Attempts remaining...\n>";
                 attempts--;
             }
         }
@@ -76,8 +71,12 @@ int main() {
         bool commandExists = false;
         for(int i = 0; i < sizeof(commands)/sizeof(commands[0]); i++) {
             if(selectedCommand == commandPrefix + commands[i]){
-                checkFile(false);
+                std::cout << "\nEnter the file path of stormworks\nDefault: C:\\Program Files\\Steam\\steamapps\\common\\Stormworks" << "\n>";
+                bool correctPath = checkFile(false);
                 commandExists = true;
+                if(correctPath == false) {
+                    help();
+                }
             }
         }
         if(commandExists == false) {
